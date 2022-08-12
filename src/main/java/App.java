a714834385e040e054ea30b7582caa7abc77ce66
Max-None <106478561+Max-None@users.noreply.github.com>
2022-08-12 12:28:03 +0300

    PP_2_1_2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+
+        System.out.println(bean==bean1);
+        System.out.println(cat==cat1);
     }
 }
