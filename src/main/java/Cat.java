93c85c74ad929978a3f6a3e33ee16d0993e0adf7
Max-None <106478561+Max-None@users.noreply.github.com>
2022-08-12 12:32:36 +0300

    PP_2_1_2

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Cat implements Serializable {
+    private String name;
+
+    public Cat() {}
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
